d7c2a5a76f712f2dc0b25a1e94b18901c3c98908
Sardor Madaminov <sardor0968@gmail.com>
2023-04-19 23:32:53 +0900

    selectors: added

--- src/components/ToDoList/ToDoList.jsx
+++ src/components/ToDoList/ToDoList.jsx
@@ -3,12 +3,15 @@ import ToDoItem from "../ToDoItem/ToDoItem";
 import ToDoItemCreator from "../ToDoItemCreator/ToDoItemCreator";
 import { todoListState } from "../Recoil/todoListState";
 import { useRecoilValue } from "recoil";
+import ToDoListFilter from "../ToDoListFilters/ToDoListFilter";
 
 function ToDoList() {
   const todoList = useRecoilValue(todoListState);
   // const { colorMode, toggleColorMode } = useColorMode();
   return (
     <>
+      <ToDoItemCreator />
+      <ToDoListFilter />
       <ToDoItemCreator />
       {todoList.map((todoItem) => (
         <ToDoItem key={todoItem.id} item={todoItem} />
